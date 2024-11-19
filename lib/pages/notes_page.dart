f772b46dae605a49998fb7cecdaafd82218cbe41
KrisnaSetia <147696945+KrisnaSetia@users.noreply.github.com>
2024-11-19 14:38:57 +0700

    jeje coomit & push

---
+++ lib/pages/notes_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NotesPage extends StatefulWidget{
+  const NotesPage ({super.key});
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
